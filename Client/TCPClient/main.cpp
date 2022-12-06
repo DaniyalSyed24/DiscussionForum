6c67a9a715f7a10e7150a0d703137cb7e3dfe15e
DaniyalSyed24 <daniyalv.syed@gmail.com>
2022-12-06 13:46:23 +0000

    added the threadpool main code and 6 args

--- Client/TCPClient/main.cpp
+++ Client/TCPClient/main.cpp
@@ -7,8 +7,8 @@
 int main(int argc, char **argv)
 {
 	// Validate the parameters
-	if (argc != 2) {
-		printf("usage: %s server-name|IP-address\n", argv[0]);
+	if (argc != 6) { // we have 5 parameters -> IP Address, Poster threads, Reader threads, Time and Throttle
+		printf("usage: %s server-name|IP-address|Poster Threads|Reader Threads|Time|Throttle\n", argv[0]);
 		return 1;
 	}
 
@@ -35,3 +35,45 @@ int main(int argc, char **argv)
 
 	return 0;
 }
+
+// IMPORTED THREADPOOL CODE
+
+#include <vector>
+#include <chrono>
+#include <string>
+
+#include "ThreadPool.h"
+
+int threadTask(int i)
+{
+	std::string str = "hello " + std::to_string(i) + "\n";
+	std::cout << str;
+	std::this_thread::sleep_for(std::chrono::seconds(3));
+	str = "world " + std::to_string(i) + "\n";
+	std::cout << str;
+	return i * i;
+}
+
+int main()
+{
+
+	ThreadPool pool(4);
+	std::vector< std::future<int> > results;
+
+	for (int i = 0; i < 8; ++i) {
+		results.emplace_back(
+			pool.enqueue(threadTask, i)
+		);
+	}
+
+	for (auto&& result : results)
+	{
+		std::string str = "result: " + std::to_string(result.get()) + "\n";
+		std::cout << str;
+	}
+
+	std::cout << std::endl;
+
+	return 0;
+}
+//////
