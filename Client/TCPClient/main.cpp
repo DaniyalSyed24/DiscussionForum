add59c7e105ccf28d6d2e26fed3226e31da646b3
DaniyalSyed24 <daniyalv.syed@gmail.com>
2022-12-10 07:16:23 +0000

    fixed client and threads + created post calculations

--- Client/TCPClient/main.cpp
+++ Client/TCPClient/main.cpp
@@ -5,6 +5,7 @@
 #include <chrono>
 #include <iostream>
 #include <string>
+#include <map>
 
 #include "TCPClient.h"
 #include "RequestGenerator.h"
@@ -18,35 +19,29 @@ void randomFunction() {
 
 std::string serverIP = "127.0.0.1";
 
-void PostRequest()
-{
-	TCPClient client(serverIP, DEFAULT_PORT);
-	std::string request;
+std::multimap<int, int> totalMessagesPerThread;
 
+void PostRequest(int threadId)
+{
+	//std::cout << "Thread " << threadId << " sent: " << std::endl;
+	// open TCP connection
+	TCPClient client(serverIP, DEFAULT_PORT);
 	client.OpenConnection();
 
-	std::vector<std::string> listOfStringsOUT;
-	//int totalPostersPerThread = 0;
-	int outerNumber = 0;
-
-	auto startMain = std::chrono::high_resolution_clock::now();	// start timer of 10seconds
-	int i = 0;
-	int totalPosterPerThread = 0;
-	int averagePerThread;
+	// generate POST requests for 1 second 10 times.
+	auto startMain = std::chrono::high_resolution_clock::now();	// start timer of 10 seconds
 	while (true)
 	{
-		RequestGenerator generator;
-		std::vector<std::string> listOfStrings;
+		RequestGenerator postGenerator;
 
-		auto start = std::chrono::high_resolution_clock::now();	// start timer
+		auto start = std::chrono::high_resolution_clock::now();	// start timer of 1 second
 		while (true) // generate random strings for 1s
 		{
-			std::string randomString = generator.generatePostRequest();
-
-			listOfStrings.push_back(randomString);
+			std::string randomString = postGenerator.generatePostRequest();
 			client.send(randomString);
+			totalMessagesPerThread.emplace(threadId, 1);
 
-			auto end = std::chrono::high_resolution_clock::now(); // Check if 1s has elapsed
+			auto end = std::chrono::high_resolution_clock::now(); // Check if 1s has elapsed, if yes -> break
 			std::chrono::duration<double, std::milli> elapsed = end - start;
 			if (elapsed.count() >= 1000)
 			{
@@ -54,57 +49,39 @@ void PostRequest()
 			}
 		}
 
-		i++;
-		std::cout << "Second " << i << ": " << listOfStrings.size() << " requests" << "\n";
-		int sizeOfList = listOfStrings.size();
-		totalPosterPerThread += sizeOfList; //total posters on a single thread.
-		std::cout << "testttt: " << totalPosterPerThread << "                     ";
-
-		int averageCounter = totalPosterPerThread /10;
-		averagePerThread = averageCounter;
-
-		auto endMain = std::chrono::high_resolution_clock::now(); // Check if 10s has elapsed
+		auto endMain = std::chrono::high_resolution_clock::now(); // Check if 10s has elapsed, if yes -> break
 		std::chrono::duration<double, std::milli> elapsedMain = endMain - startMain;
 		if (elapsedMain.count() >= 10000)
 		{
 			break;
 		}
 	}
-
-	std::cout << "\n" << "The average per thread is: " << averagePerThread;
-	
-	//int totalPostersPerThread = listOfStrings.size();
-	//std::cout << "\n" <<"Total poster request: " + std::to_string(totalPostersPerThread);
-
 	client.CloseConnection();
-
 }
+
+void ReadRequest(int threadId) {}
 
 int main(int argc, char** argv, int posters, int readers, int time, int throttle) // added parameters
 {
-
 	if (argc != 6) { // validate the parameters
 		printf("usage: %s server-name|IP-address|Poster Threads|Reader Threads|Time|Throttle\n", argv[0]);
 		return 1;
 	}
 
-	TCPClient client(argv[1], DEFAULT_PORT);
-	std::string request;
-
-	//client.OpenConnection();
-
 	int numberOfPosterThreads = std::stoi(argv[2]);
 	int numberOfReaderThreads = std::stoi(argv[3]);
 	int totalSeconds = std::stoi(argv[4]);
+
+	std::string request;
 
 	//create vectors of threads
 	std::vector< std::thread > posterThreadsVector;
 	std::vector< std::thread > readerThreadsVector;
 
 	//generate m poster threads and push them in the poster thread vector
-	for (int i = 0; i < numberOfPosterThreads; i++) 
+	for (int i = 0; i < numberOfPosterThreads; i++)
 	{
-		posterThreadsVector.push_back(std::thread(PostRequest));
+		posterThreadsVector.push_back(std::thread(PostRequest, i));
 	}
 
 	//generate n reader threads and push them in the reader thread vector
@@ -112,14 +89,26 @@ int main(int argc, char** argv, int posters, int readers, int time, int throttle
 		readerThreadsVector.push_back(std::thread());
 	}
 
+	// Wait for all threads to finish
+	for (auto& thread : posterThreadsVector) {
+		thread.join();
+	}
+	// Print out the number of messages sent by each thread
+	for (int i = 0; i < numberOfPosterThreads; i++)
+	{
+		std::cout << "Total poster requests of Thread " << i << " are: " << totalMessagesPerThread.count(i) << "\n";
+	}
 
-	// TESTING Threads connection
-	//int test = posterThreadsVector.size();
-	//int test2 = readerThreadsVector.size();
-	//std::cout << "number of posters threads " << test << "\n";
-	//std::cout << "number of reader threads: " << test2;
+	int totalPosterRequests = 0;
+	for (int i = 0; i < numberOfPosterThreads; i++)
+	{
+		totalPosterRequests = totalPosterRequests + totalMessagesPerThread.count(i);
+	}
+	std::cout << "Total poster requests: " << totalPosterRequests << ".\n";
+	std::cout << "Average requests per poster Thread: " << totalPosterRequests / numberOfPosterThreads << ".\n";
 
-	
+
+
 	do {
 		/*
 		request = "";
@@ -136,8 +125,6 @@ int main(int argc, char** argv, int posters, int readers, int time, int throttle
 		*/
 
 	} while (request != "exit" && request != "EXIT");
-
-	//client.CloseConnection();
 
 	return 0;
 }
